25c58175af13549b7f1bbe59c6a4e6181ab24d40
Fred de Borst <felison@gmx.net>
2014-01-12 22:04:07 +0100

    Delete jacklinx-dep-installer-131212-1128.sh
    Moved installer script to README

--- jacklinx-dep-installer-131212-1128.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# JackLinx
-# Script to install dependencies for JackLinx platform
-
-sudo apt-get install alacarte pulseaudio-module-jack devilspie wmctrl smbfs 9menu jackd qjackctl mididings jack-mixer qmidiroute qmidinet vmpk qsynth meterbridge kmidimon kmetronome gnome-mplayer mscore pianobooster qtractor hydrogen mixxx xsysinfo gmidimonitor patchage stopwatch aconnectgui jack-rack sysinfo rox-filer
-
